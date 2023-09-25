1ea6020230fd24b9466a551dc1e3bd5449ca52bd
Sey Kolla <shreyakolla@gmail.com>
2023-09-24 20:03:22 -0700

    got basic temperature and voltage for safemode

--- safemode.py
+++ safemode.py
@@ -1,6 +1,25 @@
 print("I am in safemode. Help!")
 import microcontroller
 import time
-time.sleep(10)
-microcontroller.reset()
+from pysquared import battery_voltage
+from Big_Data import Get_Thermo_Data
+try:
+    time.sleep(10)
+    microcontroller.reset()
+    print('in safemode')
     
+    while True:
+        voltage = battery_voltage()
+        temp = Get_Thermo_Data()
+        if(voltage > 6 & temp > -20 & temp <50):
+            break
+        
+
+        time.sleep(90)
+    microcontroller.on_next_reset(microcontroller.RunMode.NORMAL)
+    microcontroller.reset()
+
+except Exception as e:
+    time.sleep(10)
+    microcontroller.on_next_reset(microcontroller.RunMode.NORMAL)
+    microcontroller.reset()
