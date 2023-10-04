49bde7a499aeaaba9c7394a8154b35a9e4b2be13
demolishercyborg <77090326+demolishercyborg@users.noreply.github.com>
2023-10-04 16:24:45 -0700

    Update detumbletest.py
    try and except for error handling

--- detumbletest.py
+++ detumbletest.py
@@ -10,7 +10,12 @@ def debug_print(statement):
         print(co("[MAIN]" + statement, 'blue', 'bold'))
 f=functions.functions(c)
 while True:
-    f.detumble()
-    f.send("Detumble finished")
-    c.RGB=(255,255,0)
-    time.sleep(5)
+    try:
+        f.detumble()
+        f.send("Detumble finished")
+        c.RGB=(255,255,0)
+        time.sleep(5)
+    except Exception as e: 
+        debug_print(f"There is an error: {e}")
+        traceback.print_exc()
+    
