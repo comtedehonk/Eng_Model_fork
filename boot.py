31c1ae7a50429046356364de9a59be50e390bd69
Sey Kolla <shreyakolla@gmail.com>
2024-05-19 15:36:45 -0700

    updated sending protocol

--- boot.py
+++ boot.py
@@ -1,8 +1,4 @@
 import supervisor
 import storage
-
-
-# If the switch pin is connected to ground CircuitPython can write to the drive
 storage.remount("/", True)
-
 supervisor.runtime.next_stack_limit = 4096 + 4096
